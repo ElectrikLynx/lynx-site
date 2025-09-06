6f6a30ce6849c0e8aad14edd4c7503d8e7190956
Joel <joelmounts@gmail.com>
2025-09-06 19:32:27 -0400

    adds basic client section

--- UI/src/components/sections/index.js
+++ UI/src/components/sections/index.js
@@ -3,7 +3,8 @@ import ContactUs from "./contact_us/ContactUs.astro";
 import LynxTeam from "./lynx_team/LynxTeam.astro";
 import ProvidedServices from "./provided_services/ProvidedServices.astro";
 import CTA from "./call_to_action/CTA.astro";
+import Clients from "./clients/Clients.astro";
 import CostOfServices from "./cost_of_services/CostOfServices.astro";
 import ClientProfile from "./client_profile/ClientProfile.astro";
 
-export { ClientProfile, ContactUs, Hero, LynxTeam, ProvidedServices, CTA, CostOfServices };
+export { ClientProfile, ContactUs, Hero, LynxTeam, ProvidedServices, CTA, Clients, CostOfServices };
